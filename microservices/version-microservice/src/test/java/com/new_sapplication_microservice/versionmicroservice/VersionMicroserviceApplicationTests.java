ccf46e8362c91c67701a62e27ccabe4feccac085
Petar Stoyanov <59773142+PetarDimitrovStoyanov@users.noreply.github.com>
2022-05-02 19:35:04 +0300

    Add files via upload

---
+++ microservices/version-microservice/src/test/java/com/new_sapplication_microservice/versionmicroservice/VersionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.new_sapplication_microservice.versionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
