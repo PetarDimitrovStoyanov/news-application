1b7331188be0b6ade501ef8445bba50999286a10
Petar Stoyanov <59773142+PetarDimitrovStoyanov@users.noreply.github.com>
2022-05-02 19:33:38 +0300

    Add files via upload

---
+++ microservices/picture-microservice/src/test/java/com/new_sapplication_microservice/picturemicroservice/PictureMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.new_sapplication_microservice.picturemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
