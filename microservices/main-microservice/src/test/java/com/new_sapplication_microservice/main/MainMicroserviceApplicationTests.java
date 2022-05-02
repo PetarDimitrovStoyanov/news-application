1849431af6c41b3fd6169e5f3079c7805e36e2c8
Petar Stoyanov <59773142+PetarDimitrovStoyanov@users.noreply.github.com>
2022-05-02 19:32:22 +0300

    Add files via upload

---
+++ microservices/main-microservice/src/test/java/com/new_sapplication_microservice/main/MainMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.new_sapplication_microservice.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
